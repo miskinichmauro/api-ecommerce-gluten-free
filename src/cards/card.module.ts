6e6deaf4ac6a16e525c6e8d88fa27c3fa38abb36
Mauro Miskinich <miskinichmauro@gmail.com>
2025-10-05 10:34:50 -0300

    Create card.module.ts

---
+++ src/cards/card.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from './entities/cart-item.entity';
+import { CartController } from './cart.controller';
+import { CartService } from './cart.service';
+import { Product } from 'src/products/entities/product.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Cart, CartItem, Product])],
+  controllers: [CartController],
+  providers: [CartService],
+  exports: [CartService],
+})
+export class CartModule {}
