73c1ccfeb3a0796120d053d543e4138503ef5cb2
Mauro Miskinich <miskinichmauro@gmail.com>
2025-11-23 21:39:38 -0300

    Update mail.controller.ts

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { MailService } from './mail.service';
 import { SendMailDto } from './dto/send-mail.dto';
@@ -8,6 +8,7 @@ export class MailController {
   constructor(private readonly mailService: MailService) {}
 
   @Post('send')
+  @HttpCode(200)
   @ApiOperation({ summary: 'Envia un correo simple usando SendGrid' })
   send(@Body() sendMailDto: SendMailDto) {
     return this.mailService.send(sendMailDto);
